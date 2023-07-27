f3190bdf1dc4f80fe90e1e3afeaeab2be36e568e
JovSele <83567612+JovSele@users.noreply.github.com>
2023-07-27 07:04:12 +0000

    final

--- readme.txt
+++ readme.txt
@@ -1,8 +1,22 @@
-npm init
-npm i --save express
-npm i --save body-parser
-npm i --save axios
+SENDING MESSAGE TO TELEGRAM 
+- the purpose of this script is to send messages from the activity of the VPS server (node.js code) to your Telegram account
+- if the code will send continuous information from the activity, it is not necessary to check or be connected to the server(s) - everyone has their phone with them
+- it has an advantage if the code runs on several VPS servers and the information goes from all VPS servers to one Telegram account
+
+1.Create TelegramBot with @BotFather
+- recieve API_TOKEN
+- recieve CHAT_ID
+( https://www.youtube.com/watch?v=Pj8mwuMZZvg )
+
+2.Create .env file 
+-everyone put yourself TOKEN and CHAT_ID to .env file
+
+3.File info.json
+- we send the data from the given file as a message to the Telegram account
+- it is given as an example, the frequency of sending or the moment of sending can be adjusted (for example, execute the given script when an error occurs)
+- it can be modified as needed
+
+4.Install
+npm i --save puppeteer
+npm i --save fs
 npm i --save dotenv
-
-
-await page.goto('https://api.telegram.org/bot6640516640:AAF8pAWgtQbKob8T_ZB4apVgPfG3gr4RMO0/sendMessage?chat_id=5032665877&text=Hello Word from Puppeteer');
--- send.js
+++ send.js
@@ -1,13 +1,12 @@
 const puppeteer = require('puppeteer');
-const axios = require('axios');
 const fs = require('fs');
 const dotenv = require('dotenv');
 
 // Load environment variables from .env file
 dotenv.config();
 
-(async () => {
-
+// Function to send the message
+async function sendMessage() {
   try {
     // Read the content of the info.json file
     const info = fs.readFileSync('info.json', 'utf8');
@@ -16,23 +15,27 @@ dotenv.config();
     // Extract the data you want to use as the message
     const message = `Date: ${infoData.date}, Status: ${infoData.status}`;
 
-
     // Launch Puppeteer and navigate to Telegram Web
     const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
-    //await page.goto('https://api.telegram.org/bot6640516640:AAF8pAWgtQbKob8T_ZB4apVgPfG3gr4RMO0/sendMessage?chat_id=5032665877&text=Hello Word from Puppeteer');
 
     // Modify the URL to use the dynamic message
-    //const telegramUrl = `https://api.telegram.org/bot6640516640:AAF8pAWgtQbKob8T_ZB4apVgPfG3gr4RMO0/sendMessage?chat_id=5032665877&text=${encodeURIComponent(message)}`;
     const telegramUrl = `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${encodeURIComponent(message)}`;
     await page.goto(telegramUrl);
 
     console.log('Message sent successfully to Telegram group!');
 
     await browser.close();
-
   } catch (err) {
     console.error('Error occurred:', err);
   }
+}
 
-})();
+//send message imidiately
+sendMessage();
+
+/*
+// Send the message every 3 minutes
+const intervalInMinutes = 3; //here change time for resending
+setInterval(sendMessage, intervalInMinutes * 60 * 1000);
+*/
